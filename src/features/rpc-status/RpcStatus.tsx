a5fc42eebfe63d8f209d3c0d31a6c4b210421b1f
Eugene <doodleoocrypto@gmail.com>
2024-05-25 21:07:54 +0300

    update RpcStatus.tsx

--- src/features/rpc-status/RpcStatus.tsx
+++ src/features/rpc-status/RpcStatus.tsx
@@ -1,4 +1,4 @@
-import React, {memo} from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import styles from "./RpcStatus.module.scss";
 import {useTendermintNetworkParams} from "@/src/app/utils/useTendermintNetworkParams";
