6ad26348521fe0c7598b4111b380275c862905f1
Eugene <doodleoocrypto@gmail.com>
2024-05-25 21:05:35 +0300

    update StakingCalculator.tsx and styles

--- src/features/staking-calculator/StakingCalculator.tsx
+++ src/features/staking-calculator/StakingCalculator.tsx
@@ -10,7 +10,6 @@ interface Rewards {
 }
 
 const StakingCalculator: FC<OverviewContentProps> = ({ network, valueOfStakedTokens, amountOfTokens, apr }) => {
-
     const maxStake = 100000;
     const initialStake = maxStake / 2;
     const [stakeAmount, setStakeAmount] = useState<number>(initialStake);
@@ -21,6 +20,15 @@ const StakingCalculator: FC<OverviewContentProps> = ({ network, valueOfStakedTok
         const dailyReward = (amount * aprNumber) / 36500;
         const monthlyReward = dailyReward * 30;
         const yearlyReward = dailyReward * 365;
+
+        if (!apr || apr === "not available") {
+            return {
+                daily: 0,
+                monthly: 0,
+                yearly: 0,
+            };
+        }
+
         return {
             daily: Number(dailyReward.toFixed(2)),
             monthly: Number(monthlyReward.toFixed(2)),
@@ -37,17 +45,23 @@ const StakingCalculator: FC<OverviewContentProps> = ({ network, valueOfStakedTok
     return (
         <div className={styles.calculator}>
             <div className={styles.title__wrapper}>
-                {apr !== null ? (
-                    <h2 className={styles.title} style={{textAlign: "center"}}>{network.name} APR: {apr} / {stakeAmount}${network.other.ticker}</h2>
-                ) : (
-                    <div style={{ display: 'flex'}}>
-                        <h2 style={{ marginRight: '10px', textAlign: "center" }} className={styles.title}>
-                            {network.name} APR:
-                        </h2>
-                        <div className={styles.loading__container}><LoadingBlock width={100}/></div>
-                        <h2 style={{marginLeft: '10px'}}>({stakeAmount}${network.other.ticker})</h2>
-                    </div>
-                )}
+                {apr
+                    ?
+                    <>
+                        {apr === "not available"
+                            ? <h2 className={styles.title}
+                                  style={{textAlign: "center"}}>{network.name} APR: not available / {stakeAmount}${network.other.ticker}</h2>
+                            : <h2 className={styles.title}
+                                  style={{textAlign: "center"}}>{network.name} APR: {apr}% / {stakeAmount}${network.other.ticker}</h2>
+                        }
+
+                    </>
+                    :
+                    <h2 style={{marginRight: '10px', textAlign: "center"}} className={styles.title}>
+                       {network.name} APR: <span style={{color: "var(--secondary-color)"}}>{`<loading...>`}</span> / {stakeAmount}${network.other.ticker}
+                    </h2>
+                }
+
                 <p>Annual Percentage Rate</p>
             </div>
 
@@ -60,7 +74,7 @@ const StakingCalculator: FC<OverviewContentProps> = ({ network, valueOfStakedTok
                 onChange={(e) => {
                     const newValue = Number(e.target.value);
                     setStakeAmount(newValue);
-                    if (apr) {
+                    if (apr && apr !== "not available") {
                         setRewards(calculateRewards(newValue, apr));
                     }
                 }}
@@ -73,18 +87,18 @@ const StakingCalculator: FC<OverviewContentProps> = ({ network, valueOfStakedTok
 
                     {apr
                         ? <p>{rewards.daily}${network.other.ticker}</p>
-                        : <div className={styles.loading__container}><LoadingBlock width={100}/></div>
+                        : <div className={styles.loading__container}><LoadingBlock width={75}/></div>
                     }
 
                     {valueOfStakedTokens && amountOfTokens && apr
                         ?
                         <>
-                            {valueOfStakedTokens !== "not implemented"
-                                ? <p>{(rewards.daily * (valueOfStakedTokens / amountOfTokens)).toFixed(2)}$</p>
+                            {valueOfStakedTokens !== "not implemented" && apr !== "not available"
+                                ? <p>{(rewards.daily * (Number(valueOfStakedTokens) / amountOfTokens)).toFixed(2)}$</p>
                                 : <p>0$</p>
                             }
                         </>
-                        : <div className={styles.loading__container}><LoadingBlock width={100}/></div>
+                        : <div className={styles.loading__container}><LoadingBlock width={75}/></div>
                     }
                 </div>
 
@@ -93,18 +107,18 @@ const StakingCalculator: FC<OverviewContentProps> = ({ network, valueOfStakedTok
 
                     {apr
                         ? <p>{rewards.monthly}${network.other.ticker}</p>
-                        : <div className={styles.loading__container}><LoadingBlock width={100}/></div>
+                        : <div className={styles.loading__container}><LoadingBlock width={75}/></div>
                     }
 
                     {valueOfStakedTokens && amountOfTokens && apr
                         ?
                         <>
-                            {valueOfStakedTokens !== "not implemented"
-                                ? <p>{(rewards.monthly * (valueOfStakedTokens / amountOfTokens)).toFixed(2)}$</p>
+                            {valueOfStakedTokens !== "not implemented" && apr !== "not available"
+                                ? <p>{(rewards.monthly * (Number(valueOfStakedTokens) / amountOfTokens)).toFixed(2)}$</p>
                                 : <p>0$</p>
                             }
                         </>
-                        : <div className={styles.loading__container}><LoadingBlock width={100}/></div>
+                        : <div className={styles.loading__container}><LoadingBlock width={75}/></div>
                     }
                 </div>
 
@@ -113,20 +127,19 @@ const StakingCalculator: FC<OverviewContentProps> = ({ network, valueOfStakedTok
 
                     {apr
                         ? <p>{rewards.yearly}${network.other.ticker}</p>
-                        : <div className={styles.loading__container}><LoadingBlock width={100}/></div>
+                        : <div className={styles.loading__container}><LoadingBlock width={75}/></div>
                     }
 
                     {valueOfStakedTokens && amountOfTokens && apr
                         ?
                         <>
-                            {valueOfStakedTokens !== "not implemented"
-                                ? <p>{(rewards.yearly * (valueOfStakedTokens / amountOfTokens)).toFixed(2)}$</p>
+                            {valueOfStakedTokens !== "not implemented" && apr !== "not available"
+                                ? <p>{(rewards.yearly * (Number(valueOfStakedTokens) / amountOfTokens)).toFixed(2)}$</p>
                                 : <p>0$</p>
                             }
                         </>
-                        : <div className={styles.loading__container}><LoadingBlock width={100}/></div>
+                        : <div className={styles.loading__container}><LoadingBlock width={75}/></div>
                     }
-
                 </div>
             </div>
         </div>
