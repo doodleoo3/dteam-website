21cef6eebc99720bf15a218f6505457e3a43720b
Eugene <doodleoocrypto@gmail.com>
2024-05-25 21:05:06 +0300

    add 0GBuild

---
+++ src/entities/download-build-binary/ZeroGravityBuild.tsx
@@ -0,0 +1,18 @@
+import React, {FC} from 'react';
+import {TendermintContentProps} from "@/src/app/models/ITendermintContentProps";
+import ContentItem from "@/src/entities/content-item/ContentItem";
+
+const ZeroGravityBuild:FC<TendermintContentProps> = ({network, nodeVersion}) => {
+    return (
+        <ContentItem title={"BUILD BINARY"}>
+            {`cd $HOME
+git clone -b v${nodeVersion} ${network.links.git}
+./0g-chain/networks/testnet/install.sh
+source .profile
+
+${network.other.binary_name} version --long | grep -e version -e commit`}
+        </ContentItem>
+    );
+};
+
+export default ZeroGravityBuild;
