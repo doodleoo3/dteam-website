091114f0eee532e1adafa30de6970ee41191dc66
Eugene <doodleoocrypto@gmail.com>
2024-05-25 21:06:26 +0300

    update PageContainer.tsx and styles

--- src/shared/ui/page-container/PageContainer.tsx
+++ src/shared/ui/page-container/PageContainer.tsx
@@ -1,12 +1,20 @@
 'use client'
 
-import React, {FC, PropsWithChildren} from 'react';
+import React, {FC, PropsWithChildren, useEffect} from 'react';
 import styles from "./PageContainer.module.scss"
 import {useDispatch, useSelector} from "react-redux";
-import { RootState } from '@/src/app/store/store';
+import {AppDispatch, RootState} from '@/src/app/store/store';
+import {fetchMainnetNetworks, fetchTestnetNetworks} from "@/src/app/store/slices/tendermintNetworksInfoSlice";
 
 const PageContainer:FC<PropsWithChildren> = ({children}) => {
+    const dispatch: AppDispatch = useDispatch();
+
     const isOpen = useSelector((state: RootState) => state.mobileMenu.isOpen);
+
+    useEffect(() => {
+        dispatch(fetchMainnetNetworks());
+        dispatch(fetchTestnetNetworks());
+    }, [dispatch]);
 
     return (
         <main className={isOpen ?  styles.hide : styles.container}>
