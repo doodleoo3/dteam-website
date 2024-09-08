569c3d2083b18555afa3900fa634f6cfd9f33269
Eugene <doodleoocrypto@gmail.com>
2024-09-08 18:44:18 +0300

    Update WardenBuild.tsx

--- src/entities/download-build-binary/WardenBuild.tsx
+++ src/entities/download-build-binary/WardenBuild.tsx
@@ -6,7 +6,7 @@ const WardenBuild:FC<TendermintContentProps> = ({network, nodeVersion}) => {
     return (
         <ContentItem title={"BUILD BINARY"}>
             {`cd $HOME
-git clone --depth 1 --branch v${nodeVersion} ${network.links.git}
+git clone --depth 1 --branch v${nodeVersion} ${network.links.git_network_repo}
 cd wardenprotocol/warden/cmd/wardend
 go build
 mv ${network.other.binary_name} $HOME/go/bin
