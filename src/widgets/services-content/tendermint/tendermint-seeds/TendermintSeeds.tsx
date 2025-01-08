45dcd344af50b6f1dc9bc7762870f7186a7c5562
Eugene <doodleoocrypto@gmail.com>
2025-01-08 20:42:11 +0300

    Update TendermintSeeds.tsx

--- src/widgets/services-content/tendermint/tendermint-seeds/TendermintSeeds.tsx
+++ src/widgets/services-content/tendermint/tendermint-seeds/TendermintSeeds.tsx
@@ -2,20 +2,14 @@ import React, {FC} from 'react';
 import ContentItem from "@/src/entities/content-item/ContentItem";
 import {TendermintContentProps} from "@/src/app/models/ITendermintContentProps";
 import styles from "@/src/shared/ui/service-content-container/ServiceContentContainer.module.scss";
+import dynamic from "next/dynamic";
 
-const TendermintSeeds:FC<TendermintContentProps> = ({network}) => {
+const NamadaSeeds = dynamic(() => import("@/src/widgets/services-content/namada/namada-seeds/NamadaSeeds"))
+
+const TendermintSeeds:FC<TendermintContentProps> = ({network, chainId}) => {
     if (network.name === "namada") {
         return (
-            <div className={styles.container}>
-                <ContentItem title={"Seed node"}>
-                    {`SEED="${network.other.seed}"
-sed -i.bak -e "s/^seed *=.*/seed = \\"$SEED\\"/" $HOME/${network.other.working_dir}/config.toml`}
-                </ContentItem>
-                <ContentItem title={"RESTART NODE AND CHECK LOGS"}>
-                    {`sudo systemctl restart ${network.other.binary_name}
-sudo journalctl -u ${network.other.binary_name} -f -o cat`}
-                </ContentItem>
-            </div>
+            <NamadaSeeds network={network} chainId={chainId} />
         );
     }
 
