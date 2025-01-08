8cd6f323f0003d29ea1e3aed25f4ee57c815ef00
Eugene <doodleoocrypto@gmail.com>
2025-01-08 20:42:32 +0300

    Update TendermintAddrbook.tsx

--- src/widgets/services-content/tendermint/tendermint-addrbook/TendermintAddrbook.tsx
+++ src/widgets/services-content/tendermint/tendermint-addrbook/TendermintAddrbook.tsx
@@ -2,22 +2,14 @@ import React, {FC} from 'react';
 import ContentItem from "@/src/entities/content-item/ContentItem";
 import {TendermintContentProps} from "@/src/app/models/ITendermintContentProps";
 import styles from "@/src/shared/ui/service-content-container/ServiceContentContainer.module.scss";
+import dynamic from "next/dynamic";
 
-const TendermintAddrbook:FC<TendermintContentProps> = ({network}) => {
+const NamadaAddrbook = dynamic(() => import("@/src/widgets/services-content/namada/namada-addrbook/NamadaAddrbook"))
+
+const TendermintAddrbook:FC<TendermintContentProps> = ({network, chainId}) => {
     if (network.name === "namada") {
         return (
-            <div className={styles.container}>
-                <ContentItem title={"DOWNLOAD ADDRBOOK"}>
-                    {`wget -O $HOME/${network.other.working_dir}/cometbft/config/addrbook.json https://download.dteam.tech/${network.name
-                    }/${network.type}/addrbook`}
-                </ContentItem>
-                {/*<DownloadFileBlock fileName={"addrbook.json"}/>*/}
-
-                <ContentItem title={"RESTART NODE AND CHECK LOGS"}>
-                    {`sudo systemctl restart ${network.other.binary_name}
-sudo journalctl -u ${network.other.binary_name} -f -o cat`}
-                </ContentItem>
-            </div>
+            <NamadaAddrbook network={network} chainId={chainId}/>
         );
     }
 
