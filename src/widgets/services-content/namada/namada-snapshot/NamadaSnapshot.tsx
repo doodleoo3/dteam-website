259c0e67455ca73ccf3ab9ca5c8ba9aad9a52c40
Eugene <doodleoocrypto@gmail.com>
2025-01-08 20:42:45 +0300

    Update NamadaSnapshot.tsx

--- src/widgets/services-content/namada/namada-snapshot/NamadaSnapshot.tsx
+++ src/widgets/services-content/namada/namada-snapshot/NamadaSnapshot.tsx
@@ -2,35 +2,32 @@ import React, {FC} from 'react';
 import styles from "@/src/shared/ui/service-content-container/ServiceContentContainer.module.scss"
 import {TendermintContentProps} from "@/src/app/models/ITendermintContentProps";
 import ContentItem from "@/src/entities/content-item/ContentItem";
-const NamadaSnapshot:FC<TendermintContentProps> = ({network}) => {
+import SnapshotInfo from "@/src/features/snaphot-info/SnapshotInfo";
+import SnapshotTypeSelector from "@/src/features/service-type-selector/snapshot/SnapshotTypeSelector";
+const NamadaSnapshot:FC<TendermintContentProps> = ({network, snapshotType, chainId}) => {
     return (
-        <div className={styles.container}>
-            <ContentItem title={"Stop service"}>
-                {`sudo systemctl stop namadad`}
-            </ContentItem>
-
-            <ContentItem title={"Make backup and reset db"}>
-                {`cp $HOME/.local/share/namada/shielded-expedition.88f17d1d14/cometbft/data/priv_validator_state.json $HOME/.local/share/namada/shielded-expedition.88f17d1d14/priv_validator_state.json.backup
-rm -rf $HOME/.local/share/namada/shielded-expedition.88f17d1d14/cometbft/data $HOME/.local/share/namada/shielded-expedition.88f17d1d14/db`}
-            </ContentItem>
-
-            <ContentItem title={"Download snapshot"}>
-                {`curl -L https://snapshots.lavenderfive.com/testnet-extra-db/namada/latest_db.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.local/share/namada
-
-curl -L https://snapshots.lavenderfive.com/testnet-snapshots/namada/latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.local/share/namada`}
-            </ContentItem>
-
-            <ContentItem title={"Move snapshot and backup"}>
-                {`mv "$HOME/.local/share/namada/data/" "$HOME/.local/share/namada/shielded-expedition.88f17d1d14/cometbft/"
-mv "$HOME/.local/share/namada/db/" "$HOME/.local/share/namada/shielded-expedition.88f17d1d14/"
-
-cp $HOME/.local/share/namada/shielded-expedition.88f17d1d14/priv_validator_state.json.backup $HOME/.local/share/namada/shielded-expedition.88f17d1d14/cometbft/data/priv_validator_state.json`}
-            </ContentItem>
-
-            <ContentItem title={"Restart service and check logs"}>
-                {`sudo systemctl restart namadad
-sudo journalctl -u namadad -f`}
-            </ContentItem>
+        <div className={styles.container__with__types}>
+            <SnapshotInfo network={network} snapshotType={snapshotType}/>
+            <SnapshotTypeSelector network={network}/>
+            <div className={styles.types__content__container}>
+                <ContentItem title={"INSTALL DEPENDENCIES"}>
+                    {`sudo apt update
+sudo apt-get install snapd lz4 -y`}
+                </ContentItem>
+                <ContentItem title={"STOP NODE AND RESET DATA"}>
+                    {`sudo systemctl stop ${network.other.binary_name}
+cp $HOME/${network.other.working_dir}/${chainId}/cometbft/data/priv_validator_state.json $HOME/${network.other.working_dir}/${chainId}/priv_validator_state.json.backup
+rm -rf $HOME/${network.other.working_dir}/${chainId}/cometbft/data $HOME/${network.other.working_dir}/${chainId}/{db,wasm}`}
+                </ContentItem>
+                <ContentItem title={"DOWNLOAD SNAPSHOT"}>
+                    {`curl -o - -L https://download.dteam.tech/${network.name}/${network.type}/latest-snapshot  | lz4 -c -d - | tar -x -C $HOME/${network.other.working_dir}/${chainId}
+mv $HOME/${network.other.working_dir}/${chainId}/priv_validator_state.json.backup $HOME/${network.other.working_dir}/${chainId}/cometbft/data/priv_validator_state.json`}
+                </ContentItem>
+                <ContentItem title={"RESTART NODE AND CHECK LOGS"}>
+                    {`sudo systemctl restart ${network.other.binary_name}
+sudo journalctl -u ${network.other.binary_name} -f -o cat`}
+                </ContentItem>
+            </div>
         </div>
     );
 };
