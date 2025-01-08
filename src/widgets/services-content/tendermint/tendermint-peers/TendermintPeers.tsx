79590554b4c22bd7c18e2864027e2afb5c9359c6
Eugene <doodleoocrypto@gmail.com>
2025-01-08 20:42:18 +0300

    Update TendermintPeers.tsx

--- src/widgets/services-content/tendermint/tendermint-peers/TendermintPeers.tsx
+++ src/widgets/services-content/tendermint/tendermint-peers/TendermintPeers.tsx
@@ -3,27 +3,16 @@ import ContentItem from "@/src/entities/content-item/ContentItem";
 import {TendermintContentProps} from "@/src/app/models/ITendermintContentProps";
 import styles from "@/src/shared/ui/service-content-container/ServiceContentContainer.module.scss";
 import {useTendermintNetworkParams} from "@/src/app/utils/useTendermintNetworkParams";
+import dynamic from "next/dynamic";
 
-const TendermintPeers:FC<TendermintContentProps> = ({network}) => {
+const NamadaPeers = dynamic(() => import("@/src/widgets/services-content/namada/namada-peers/NamadaPeers"))
+
+const TendermintPeers:FC<TendermintContentProps> = ({network, chainId}) => {
     const networkParams = useTendermintNetworkParams(network.name, network.type);
 
     if (network.name === "namada") {
         return (
-            <div className={styles.container}>
-                <ContentItem title={"Live peers"}>
-                    {`${networkParams?.peers
-                        ?
-                        `PEERS="${network.other.peer},${networkParams?.peers}"`
-                        :
-                        `PEERS="${network.other.peer}"`
-                    }
-sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \\"$PEERS\\"/" $HOME/${network.other.working_dir}/config.toml`}
-                </ContentItem>
-                <ContentItem title={"RESTART NODE AND CHECK LOGS"}>
-                    {`sudo systemctl restart ${network.other.binary_name}
-sudo journalctl -u ${network.other.binary_name} -f -o cat`}
-                </ContentItem>
-            </div>
+            <NamadaPeers network={network} chainId={chainId} />
         );
     }
 
