0a542fe7e2f20dc413282e94c7c32fff6ba09a27
Eugene <doodleoocrypto@gmail.com>
2025-01-08 20:42:27 +0300

    Update TendermintGenesis.tsx

--- src/widgets/services-content/tendermint/tendermint-genesis/TendermintGenesis.tsx
+++ src/widgets/services-content/tendermint/tendermint-genesis/TendermintGenesis.tsx
@@ -2,23 +2,14 @@ import React, {FC} from 'react';
 import ContentItem from "@/src/entities/content-item/ContentItem";
 import {TendermintContentProps} from "@/src/app/models/ITendermintContentProps";
 import styles from "@/src/shared/ui/service-content-container/ServiceContentContainer.module.scss";
+import dynamic from "next/dynamic";
 
-const TendermintGenesis:FC<TendermintContentProps> = ({network}) => {
+const NamadaGenesis = dynamic(() => import("@/src/widgets/services-content/namada/namada-genesis/NamadaGenesis"))
+
+const TendermintGenesis:FC<TendermintContentProps> = ({network, chainId}) => {
     if (network.name === "namada") {
         return (
-            <div className={styles.container}>
-                <ContentItem title={"DOWNLOAD GENESIS"}>
-                    {`wget -O $HOME/${network.other.working_dir}/cometbft/config/genesis.json https://download.dteam.tech/${network.name
-                    }/${network.type}/genesis`}
-                </ContentItem>
-
-                {/*<DownloadFileBlock fileName={"genesis.json"}/>*/}
-
-                <ContentItem title={"RESTART NODE AND CHECK LOGS"}>
-                    {`sudo systemctl restart ${network.other.binary_name}
-sudo journalctl -u ${network.other.binary_name} -f -o cat`}
-                </ContentItem>
-            </div>
+            <NamadaGenesis network={network} chainId={chainId} />
         );
     }
 
