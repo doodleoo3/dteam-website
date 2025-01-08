eca35f29af02edd9fa562fe81c71cefdd7b051cf
Eugene <doodleoocrypto@gmail.com>
2025-01-08 20:45:49 +0300

    Create NamadaAddrbook.tsx

---
+++ src/widgets/services-content/namada/namada-addrbook/NamadaAddrbook.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from 'react';
+import styles from "@/src/shared/ui/service-content-container/ServiceContentContainer.module.scss";
+import ContentItem from "@/src/entities/content-item/ContentItem";
+import {TendermintContentProps} from "@/src/app/models/ITendermintContentProps";
+
+const NamadaAddrbook:FC<TendermintContentProps> = ({network, chainId}) => {
+    return (
+        <div className={styles.container}>
+            <ContentItem title={"DOWNLOAD ADDRBOOK"}>
+                {`wget -O $HOME/${network.other.working_dir}/${chainId}/cometbft/config/addrbook.json https://download.dteam.tech/${network.name
+                }/${network.type}/addrbook`}
+            </ContentItem>
+
+            <ContentItem title={"RESTART NODE AND CHECK LOGS"}>
+                {`sudo systemctl restart ${network.other.binary_name}
+sudo journalctl -u ${network.other.binary_name} -f -o cat`}
+            </ContentItem>
+        </div>
+    );
+};
+
+export default NamadaAddrbook;
