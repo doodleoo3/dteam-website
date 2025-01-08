339274e46fff03df049dce313a2777d4507dd5e7
Eugene <doodleoocrypto@gmail.com>
2025-01-08 20:42:52 +0300

    Update LavaDownload.tsx

--- src/entities/download-build-binary/LavaDownload.tsx
+++ src/entities/download-build-binary/LavaDownload.tsx
@@ -6,7 +6,7 @@ const LavaDownload:FC<TendermintContentProps> = ({network, nodeVersion}) => {
     return (
         <ContentItem title={"DOWNLOAD BINARY"}>
             {`cd $HOME
-wget -O ${network.other.binary_name} ${network.links.binary_download}/v${nodeVersion}/${network.other.binary_name}-${nodeVersion}-linux-amd64
+wget -O ${network.other.binary_name} ${network.links.binary_download}/v${nodeVersion}/${network.other.binary_name}-v${nodeVersion}-linux-amd64
 chmod +x $HOME/${network.other.binary_name}
 mv $HOME/${network.other.binary_name} $HOME/go/bin/${network.other.binary_name}
 
